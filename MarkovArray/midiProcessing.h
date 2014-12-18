c6d32b4a51daf6e3a5ae567e8e2562203a527653
LukasNowok <lukasfranknowok@hotmail.de>
2014-12-18 11:50:41 +0100

    aenderungTest

--- MarkovArray/midiProcessing.h
+++ MarkovArray/midiProcessing.h
@@ -12,7 +12,7 @@
 class midiProcessing
 {
 private:
-    std::vector<int> trackData{1, 2, 3};
+    std::vector<int> trackData{1, 2, 3, 4, 5, 6};
     std::vector< std::vector<int> > markovList;
     std::vector<int> tempVector;
 public:
